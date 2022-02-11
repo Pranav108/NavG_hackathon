b2df871e523a21023cc9955963f26c639b465050
Pranav Pratap Singh <56934594+Pranav108@users.noreply.github.com>
2022-02-12 02:35:50 +0530

    Create script.js

---
+++ React/DOM/script.js
@@ -0,0 +1,8 @@
+'use strict';
+/* This one is yourt last task here
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+*/
