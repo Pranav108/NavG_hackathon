fe9f070f57101b5582e42c907e1990d8a2e77223
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-03-05 10:25:34 +0530

    fixed folder issue

---
+++ 02_JS_5challenges/04-count-sequences.js
@@ -0,0 +1,27 @@
+
+// count sequences, three numbers, first equals to third
+// and middle one different, all three numbers equal does not count [4,4,4]
+
+const first = [8, 6, 8, 6, 7, 4, 7]
+// [8,6,8], [6,8,6], [7,4,7];
+const second = [4, 5, 5, 7, 5, 5, 9]
+// [5,7,6]
+const third = [3, 3, 2, 8, 7, 6, 5]
+//
+
+//reduce this code to 2 lines of code
+// const count = (data) => {
+//   return data.reduce((total, item, index) => {
+//     console.log(item)
+//     console.log(item === data[index + 2])
+//     console.log(item !== data[index + 1])
+//     const match = item === data[index + 2] && item !== data[index + 1]
+//     if (match) {
+//       return total + 1
+//     }
+//     return total
+//   }, 0)
+// }
+
+count(first)
+console.log(count(second))
