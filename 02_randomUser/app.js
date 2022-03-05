2cced6e012530a6ea1e7b6a9ab2ed4a93a685509
Karishma Ahirwal <karishmaahirwal3532gmail.com>
2022-03-06 05:11:44 +0530

    initial massage

--- 02_randomUser/app.js
+++ 02_randomUser/app.js
@@ -1,1 +1,15 @@
 //Happy holi
+
+const firstname=["hope","launa","katherine","walker","moses","tayna","rosia","yahaira","tommy","elwands",
+"clorinda","sheron","kayla","clementina","rene","rex","kathy","latoya","shirleena","shoshana"];
+
+const secondname=["chauncey","houchins","lama","frasca","houston","ake","shankles","cantor",
+"Mizell","cleland","maltby","Tirrell","cary","Mace",'Horta',"carlile","Deegan","Torrez","humfeld","helgeson"];
+
+const getRandomNumber=(max)=>Math.floor(Math.random()* max);
+
+const getRandomNumber=()=>'${firstname[getRandomNumber(firstname.length)]}${secondname.length)]}';
+
+const getRandomNumber=()=>
+console.log(getRandomNumber())
+
