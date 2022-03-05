956c73ac80bd9bbf811af695e4834c7be00a26ec
kajal8512 <kajaly20@navgurukul.org>
2022-03-06 04:09:54 +0530

    inital commit

--- 02_JS_5challenges/02-multiply-args.js
+++ 02_JS_5challenges/02-multiply-args.js
@@ -12,14 +12,19 @@
 
 // multiply(2, 3, 4, 5, 6, 7)
 
-console.log(multiply(4, 5))
+// console.log(multiply(4, 5))console.log(multiply(4, 5))
 // 4 * 5 = 20
-console.log(multiply(1, 2, 3, 5))
+// console.log(multiply(1, 2, 3, 5))
 // 1 * 2 * 3 * 5 = 30
-console.log(multiply(2, 0, 4, 2))
+// console.log(multiply(2, 0, 4, 2))
 // 2 + 0 * 4 * 2
 
 //  NIFTY SOLUTIONS
 
-// const multiply = (...args) =>
-//   args.reduce((total, item) => total * (item || 1), 1)
+const multiply = (...args) =>{
+  const res=args.reduce((total, item) => total * (item || 1), 1)
+  return res
+}
+
+console.log(multiply(4, 5))
+
