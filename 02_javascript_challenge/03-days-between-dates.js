94d9f5b8a0cdfb67d8287fbdbed4c3fddec39029
Pranav Pratap Singh <56934594+Pranav108@users.noreply.github.com>
2022-03-05 10:21:13 +0530

    fixed white arrow issue with JS_challenge folder

---
+++ 02_javascript_challenge/03-days-between-dates.js
@@ -0,0 +1,18 @@
+const date1 = new Date('May 27, 2024')
+const date2 = new Date('Jan 5 , 2022')
+
+//reduce this to a single arrow function
+// const getDays = (date1,date2)=>{
+//   const oneDay = 1000 * 60 * 60 * 24;
+//   const time2 = date2.getTime();
+//   console.log(time2);
+//   const time1 = date1.getTime();
+//   console.log(time1);
+//   let time = time2 - time1;
+//   time = time / oneDay;
+//   time = Math.round(Math.abs(time))
+// return time
+// }
+
+
+console.log(getDays(date1, date2))
